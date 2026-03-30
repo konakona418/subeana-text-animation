ac174dd81d6731f151efa7fb9b36303c0537e5e1
konakona418 <creeperblin@outlook.com>
2026-03-30 18:43:50 +0800

    add fancy bloom vfx

--- main.c
+++ main.c
@@ -26,6 +26,43 @@ static Font*     default_font;
 static const int   s_spacing           = 4;
 static const float s_scatter_magnitude = 300.f;
 static const float gravity             = 5.f;
+static const int   s_bloom_levels      = 4;
+static const float s_bloom_strength    = 0.8f;
+
+static const char s_bloom_downsample_shader[] =
+  "#version 330 core\n"
+  "in vec2 fragTexCoord;\n"
+  "in vec4 fragColor;\n"
+  "uniform sampler2D texture0;\n"
+  "uniform vec4 colDiffuse;\n"
+  "uniform vec2 texelSize;\n"
+  "out vec4 finalColor;\n"
+  "void main()\n"
+  "{\n"
+  "  vec3 sum = vec3(0.0);\n"
+  "  for (int x = -1; x <= 1; x++)\n"
+  "  {\n"
+  "    for (int y = -1; y <= 1; y++)\n"
+  "    {\n"
+  "      sum += texture(texture0, fragTexCoord + vec2(x, y) * texelSize).rgb;\n"
+  "    }\n"
+  "  }\n"
+  "  vec3 blurred = sum/9.0;\n"
+  "  finalColor = vec4(blurred, 1.0) * colDiffuse;\n"
+  "}\n";
+
+static const char s_bloom_upsample_shader[] =
+  "#version 330 core\n"
+  "in vec2 fragTexCoord;\n"
+  "in vec4 fragColor;\n"
+  "uniform sampler2D texture0;\n"
+  "uniform vec4 colDiffuse;\n"
+  "out vec4 finalColor;\n"
+  "void main()\n"
+  "{\n"
+  "  vec4 color = texture(texture0, fragTexCoord);\n"
+  "  finalColor = color * colDiffuse;\n"
+  "}\n";
 
 #define FreeBuffer(buffer) \
   do {                     \
@@ -285,18 +322,81 @@ int MeasureUtf32SVWidth(Font font, Utf32SV text) {
   return width;
 }
 
+void RenderBloomPass(
+  RenderTexture  render_texture,
+  RenderTexture* bloom_textures,
+  const int*     bloom_widths,
+  const int*     bloom_heights,
+  int            bloom_levels,
+  Shader         downsample_shader,
+  Shader         upsample_shader,
+  int            downsample_texel_loc) {
+  Texture source_texture = render_texture.texture;
+  int     source_width   = render_texture.texture.width;
+  int     source_height  = render_texture.texture.height;
+
+  for (int i = 0; i < bloom_levels; ++i) {
+    BeginTextureMode(bloom_textures[i]);
+    BeginShaderMode(downsample_shader);
+    {
+      Vector2 texel_size = {1.0f / (float) source_width, 1.0f / (float) source_height};
+      SetShaderValue(downsample_shader, downsample_texel_loc, &texel_size, SHADER_UNIFORM_VEC2);
+      DrawTexturePro(
+        source_texture,
+        (Rectangle){0.0f, 0.0f, (float) source_width, (float) -source_height},
+        (Rectangle){0.0f, 0.0f, (float) bloom_widths[i], (float) bloom_heights[i]},
+        (Vector2){0.0f, 0.0f},
+        0.0f,
+        WHITE);
+    }
+    EndShaderMode();
+    EndTextureMode();
+
+    source_texture = bloom_textures[i].texture;
+    source_width   = bloom_widths[i];
+    source_height  = bloom_heights[i];
+  }
+
+  for (int i = bloom_levels - 2; i >= 0; --i) {
+    BeginTextureMode(bloom_textures[i]);
+    BeginBlendMode(BLEND_ADDITIVE);
+    BeginShaderMode(upsample_shader);
+    DrawTexturePro(
+      bloom_textures[i + 1].texture,
+      (Rectangle){0.0f, 0.0f, (float) bloom_widths[i + 1], (float) -bloom_heights[i + 1]},
+      (Rectangle){0.0f, 0.0f, (float) bloom_widths[i], (float) bloom_heights[i]},
+      (Vector2){0.0f, 0.0f},
+      0.0f,
+      WHITE);
+    EndShaderMode();
+    EndBlendMode();
+    EndTextureMode();
+  }
+}
+
 GlyphEntity** glyph_entities;
 
 void DrawAnim(Utf32SV text) {
-  const int           screen_width  = 800;
-  const int           screen_height = 600;
-  Font                font;
-  PSlugFont           slug_font;
-  Utf32SV*            lines;
-  size_t              line_count;
+  const int           screen_width         = 800;
+  const int           screen_height        = 600;
+  Font                font                 = {0};
+  PSlugFont           slug_font            = NULL;
+  RenderTexture       render_texture       = {0};
+  Shader              downsample_shader    = {0};
+  Shader              upsample_shader      = {0};
+  int                 downsample_texel_loc = -1;
+  RenderTexture       bloom_textures[4]    = {0};
+  int                 bloom_widths[4]      = {0};
+  int                 bloom_heights[4]     = {0};
+  int                 current_width;
+  int                 current_height;
+  Utf32SV*            lines      = NULL;
+  size_t              line_count = 0;
   CompoundGlyphEntity compound_entity;
   int                 line_width;
   int                 y_offset;
+
+  glyph_entities = NULL;
 
   PrintUtf32SV(text);
 
@@ -309,6 +409,24 @@ void DrawAnim(Utf32SV text) {
   SplitUTF8IntoLines(text, lines);
 
   InitWindow(screen_width, screen_height, "Subeana-style Text Animation");
+
+  render_texture = LoadRenderTexture(screen_width, screen_height);
+  SetTextureFilter(render_texture.texture, TEXTURE_FILTER_BILINEAR);
+
+  downsample_shader    = LoadShaderFromMemory(NULL, s_bloom_downsample_shader);
+  upsample_shader      = LoadShaderFromMemory(NULL, s_bloom_upsample_shader);
+  downsample_texel_loc = GetShaderLocation(downsample_shader, "texelSize");
+
+  current_width  = screen_width;
+  current_height = screen_height;
+  for (int i = 0; i < s_bloom_levels; ++i) {
+    current_width     = (current_width > 1) ? current_width / 2 : 1;
+    current_height    = (current_height > 1) ? current_height / 2 : 1;
+    bloom_widths[i]   = current_width;
+    bloom_heights[i]  = current_height;
+    bloom_textures[i] = LoadRenderTexture(current_width, current_height);
+    SetTextureFilter(bloom_textures[i].texture, TEXTURE_FILTER_BILINEAR);
+  }
 
   font = LoadFontEx(
     FONT_FILE,
@@ -345,7 +463,7 @@ void DrawAnim(Utf32SV text) {
   }
 
   while (!WindowShouldClose()) {
-    BeginDrawing();
+    BeginTextureMode(render_texture);
     ClearBackground(BLACK);
 
     b2World_Step(world, 1 / 60.f, 4);
@@ -431,19 +549,64 @@ void DrawAnim(Utf32SV text) {
       }
     }
 
+    EndTextureMode();
+
+    RenderBloomPass(
+      render_texture,
+      bloom_textures,
+      bloom_widths,
+      bloom_heights,
+      s_bloom_levels,
+      downsample_shader,
+      upsample_shader,
+      downsample_texel_loc);
+
+    BeginDrawing();
+    ClearBackground(BLACK);
+
+    DrawTextureRec(
+      render_texture.texture,
+      (Rectangle){0.0f, 0.0f, (float) render_texture.texture.width, (float) -render_texture.texture.height},
+      (Vector2){0.0f, 0.0f},
+      WHITE);
+
+    BeginBlendMode(BLEND_ADDITIVE);
+    DrawTexturePro(
+      bloom_textures[0].texture,
+      (Rectangle){0.0f, 0.0f, (float) bloom_widths[0], (float) -bloom_heights[0]},
+      (Rectangle){0.0f, 0.0f, (float) screen_width, (float) screen_height},
+      (Vector2){0.0f, 0.0f},
+      0.0f,
+      (Color){255, 255, 255, (unsigned char) (255.0f * s_bloom_strength)});
+    EndBlendMode();
+
     EndDrawing();
   }
 
   CloseWindow();
 
 cleanup:
-  for (size_t i = 0; i < line_count - 1; ++i) {
-    FreeGlyphEntities(glyph_entities[i], lines[i + 1].length);
+  if (glyph_entities != NULL) {
+    for (size_t i = 0; i < line_count - 1; ++i) {
+      FreeGlyphEntities(glyph_entities[i], lines[i + 1].length);
+    }
+    MemFree(glyph_entities);
   }
-  MemFree(glyph_entities);
 
-  UnloadFont(font);
+  for (int i = 0; i < s_bloom_levels; ++i) {
+    if (bloom_textures[i].id > 0) UnloadRenderTexture(bloom_textures[i]);
+  }
+
+  if (downsample_shader.id > 0) UnloadShader(downsample_shader);
+  if (upsample_shader.id > 0) UnloadShader(upsample_shader);
+  if (render_texture.id > 0) UnloadRenderTexture(render_texture);
+
+  if (slug_font != NULL) UnloadFontSlug(slug_font);
+  if (font.texture.id > 0) UnloadFont(font);
+
   FreeBuffer(lines);
+
+  if (IsWindowReady()) CloseWindow();
 }
 
 int main(int argc, char** argv) {
